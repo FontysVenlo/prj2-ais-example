1e9a828d7a6cbad8b9dc88993329ba878067f525
Bonajo <m.bonajo@fontys.nl>
2021-05-11 14:07:53 +0200

    Pipe error to null

--- doc/images/convertDarkMode.sh
+++ doc/images/convertDarkMode.sh
@@ -12,12 +12,12 @@ then
         # Make sure it does not have the required css
         if ! grep -q 'prefers-color-scheme' "$file"; then
             # Replace black with currentColor and add required css
-            sed -i '/^<svg/s/black/currentColor/g;s/<\/svg/<style> \*{color:#000\;}@media (prefers-color-scheme: dark){\*{color: #fff\;}}<\/style><\/svg/' "$file"
+            sed -i '/^<svg/s/black/currentColor/g;s/<\/svg/<style> \*{color:#000\;}@media (prefers-color-scheme: dark){\*{color: #fff\;}}<\/style><\/svg/' "$file";
         fi
     done
 else
     # One liner that finds all svg files in the current directory
     # Checks if the file already have the correct css applied
     # Otherwise change the stroke from black to currentColor and add the correct css
-    find . -name '*.svg' | xargs grep -riL 'prefers-color-scheme' | xargs sed -i '/^<svg/s/black/currentColor/g;s/<\/svg/<style> \*{color:#000\;}@media (prefers-color-scheme: dark){\*{color: #fff\;}}<\/style><\/svg/';
+    find . -name '*.svg' | xargs grep -riL 'prefers-color-scheme' | xargs sed -i '/^<svg/s/black/currentColor/g;s/<\/svg/<style> \*{color:#000\;}@media (prefers-color-scheme: dark){\*{color: #fff\;}}<\/style><\/svg/' 2>/dev/null;
 fi

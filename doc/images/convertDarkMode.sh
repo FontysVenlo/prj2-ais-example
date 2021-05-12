24f31bdf29d567e9d6b4bdb156e710431b3ae50b
Martijn Bonajo <m.bonajo@fontys.nl>
2021-05-12 10:06:42 +0200

    Update script for inverse and mac

--- doc/images/convertDarkMode.sh
+++ doc/images/convertDarkMode.sh
@@ -12,12 +12,13 @@ then
         # Make sure it does not have the required css
         if ! grep -q 'prefers-color-scheme' "$file"; then
             # Replace black with currentColor and add required css
-            sed -i '/^<svg/s/black/currentColor/g;s/<\/svg/<style> \*{color:#000\;}@media (prefers-color-scheme: dark){\*{color: #fff\;}}<\/style><\/svg/' "$file";
+            sed -i 's/<\/svg/<style>@media (prefers-color-scheme: dark){svg{filter: invert(100)\;}}<\/style><\/svg/' "$file";
+            #/^<svg/s/black/currentColor/g;
         fi
     done
 else
     # One liner that finds all svg files in the current directory
     # Checks if the file already have the correct css applied
     # Otherwise change the stroke from black to currentColor and add the correct css
-    find . -name '*.svg' | xargs grep -riL 'prefers-color-scheme' | xargs sed -i '/^<svg/s/black/currentColor/g;s/<\/svg/<style> \*{color:#000\;}@media (prefers-color-scheme: dark){\*{color: #fff\;}}<\/style><\/svg/' 2>/dev/null;
+    find . -name '*.svg' | xargs grep -riL 'prefers-color-scheme' | xargs sed -i '' 's/<\/svg/<style>@media (prefers-color-scheme: dark){svg{filter: invert(100)\;}}<\/style><\/svg/' 2>/dev/null;
 fi
